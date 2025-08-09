3b38a4f2d1a9c667cf44f0f23ea0ae3d0b3cc25d
Alejandro Heredia <logusgraphics@icloud.com>
2025-08-09 14:24:02 +0200

    chore: detaching relationship on delete organization project

--- components/features/projects/DeleteProjectDialog.tsx
+++ components/features/projects/DeleteProjectDialog.tsx
@@ -1,17 +1,25 @@
 'use client';
 
 import { DeleteDialog } from '@/components/common';
+import { useOrganizationProjectMutations } from '@/hooks';
 import { useProjectMutations } from '@/hooks/projects';
+import { useOrganizationsStore } from '@/stores/organizations.store';
 import { useProjectsStore } from '@/stores/projects.store';
 
 export function DeleteProjectDialog() {
   const projectToDelete = useProjectsStore((state) => state.projectToDelete);
+  const selectedOrganizationId = useOrganizationsStore((state) => state.selectedOrganizationId);
   const setProjectToDelete = useProjectsStore((state) => state.setProjectToDelete);
   const { deleteProject } = useProjectMutations();
+  const { removeOrganizationProject } = useOrganizationProjectMutations();
 
   const handleDelete = async () => {
     if (!projectToDelete) return;
     await deleteProject(projectToDelete.id, projectToDelete.name);
+    await removeOrganizationProject({
+      organizationId: selectedOrganizationId as string,
+      projectId: projectToDelete.id,
+    });
     setProjectToDelete(null);
   };
 
